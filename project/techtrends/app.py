a7c584bcc8d94b159261ddaf209c6ba1caeb3ce2
arunprakashpj <arunprakashpj@gmail.com>
2021-08-20 21:58:11 +0200

    App Log file is not updated

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -94,5 +94,13 @@ def metrics():
 # start the application on port 3111
 if __name__ == "__main__":
    #logging.basicConfig(filename='app.log',level=logging.DEBUG)
-   logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+   #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+   logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ]
+)
    app.run(debug=True, host='0.0.0.0', port='3111')
